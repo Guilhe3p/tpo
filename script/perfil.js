e1a19979c5e4a530afedeb628801d88be349be18
Guilhe3p <guilhermerutenberg@gmail.com>
2023-07-10 15:10:51 -0300

    perfil funcional sin estilo

--- script/perfil.js
+++ script/perfil.js
@@ -8,6 +8,25 @@ function CargarDatos(){
     document.getElementById("user-name").innerHTML = USUARIO.nombre
     document.getElementById("user-fnas").innerHTML = USUARIO.f_nas
     document.getElementById("user-mail").innerHTML = USUARIO.correo
+
+    if (USUARIO.imagen != ""){
+        document.getElementById("agregar-foto").style.display = "none"
+        document.getElementById("foto-perfil").src = "img/"+USUARIO.imagen
+    }
+
+    puntuaciones = document.getElementById("puntuados")
+    if (Object.keys(USUARIO.puntuados) = []){
+        puntuaciones.innerHTML = "<h1>Parece que no tienes juegos puntuados todavía!</h1>"
+    }
+    for (let puntuado in Object.keys(USUARIO.puntuados)){
+        puntuaciones.innerHTML += `
+            <div class="juego-puntuado">
+                <span><h1>{puntuado}</h1></span><span><h1>{USUARIO.puntuados[puntuado]}</h1></span>
+            </div>
+            `
+    }
 }
+
+
 
 CargarDatos()
